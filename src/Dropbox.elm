fc3e93ac588391a7675e30490f7285e1b06e6dea
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-05-26 22:45:17 -0700

    Add all return values for upload

--- src/Dropbox.elm
+++ src/Dropbox.elm
@@ -1,11 +1,19 @@
 module Dropbox
     exposing
         ( AuthorizeRequest
+        , Dimensions
         , DownloadRequest
         , DownloadResponse
+        , FileSharingInfo
+        , GpsCoordinates
+        , MediaInfo
+        , MediaMetadata
+        , PhotoMetadata
+        , PropertyGroup
         , UploadRequest
         , UploadResponse
         , UserAuth
+        , VideoMetadata
         , WriteMode(..)
         , authFromLocation
         , authorizationUrl
@@ -40,16 +48,18 @@ See the official Dropbox documentation at
 ### Files
 
 @docs download, DownloadRequest, DownloadResponse
-@docs upload, UploadRequest, WriteMode, UploadResponse
+@docs upload, UploadRequest, WriteMode, UploadResponse, MediaInfo, MediaMetadata, PhotoMetadata, VideoMetadata, Dimensions, GpsCoordinates, FileSharingInfo, PropertyGroup
 
 -}
 
 import Date exposing (Date)
 import Date.Format
-import Dict
+import Dict exposing (Dict)
 import Html exposing (Html)
 import Http
 import Json.Decode
+import Json.Decode.Extra
+import Json.Decode.Pipeline as Pipeline
 import Json.Encode
 import Navigation
 import Update.Extra
@@ -296,10 +306,170 @@ type alias UploadRequest =
     }
 
 
+{-| Additional information if the file is a photo or video.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type MediaInfo
+    = Pending
+    | Metadata MediaMetadata
+
+
+decodeMediaInfo : Json.Decode.Decoder MediaInfo
+decodeMediaInfo =
+    -- TODO
+    Json.Decode.succeed Pending
+
+
+{-| Metadata for a photo or video.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type MediaMetadata
+    = Photo PhotoMetadata
+    | Video VideoMetadata
+
+
+{-| Metadata for a photo.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type alias PhotoMetadata =
+    { dimensions : Maybe Dimensions
+    , location : Maybe GpsCoordinates
+    , timeTaken : Maybe Date
+    }
+
+
+{-| Metadata for a video.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+**WARNING**: elm-dropbox may give the incorrect values for `duration`,
+since Elm currently does not provide a way to parse and represent 64-bit integers.
+
+-}
+type alias VideoMetadata =
+    { dimensions : Maybe Dimensions
+    , location : Maybe GpsCoordinates
+    , timeTaken : Maybe Date
+    , duration : Maybe Int -- XXX: should be UInt64
+    }
+
+
+{-| Dimensions for a photo or video.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+**WARNING**: elm-dropbox may give the incorrect values,
+since Elm currently does not provide a way to parse and represent 64-bit integers.
+
+-}
+type alias Dimensions =
+    { height : Int -- XXX: should be UInt64
+    , width : Int -- XXX: should be UInt64
+    }
+
+
+{-| The GPS coordinate of the photo/video.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type alias GpsCoordinates =
+    { latitude : Float
+    , longitude : Float
+    }
+
+
+{-| Sharing info for a file which is contained by a shared folder.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type alias FileSharingInfo =
+    { readOnly : Bool
+    , parentSharedFolderId : String
+    , modifiedBy : Maybe String
+    }
+
+
+decodeFileSharingInfo : Json.Decode.Decoder FileSharingInfo
+decodeFileSharingInfo =
+    Pipeline.decode FileSharingInfo
+        |> Pipeline.required "read_only" Json.Decode.bool
+        |> Pipeline.required "parent_shared_folder_id" Json.Decode.string
+        |> Pipeline.optional "modified_by" (Json.Decode.nullable Json.Decode.string) Nothing
+
+
+{-| Collection of custom properties in filled property templates.
+
+See <https://www.dropbox.com/developers/documentation/http/documentation#files-upload>
+
+-}
+type alias PropertyGroup =
+    { templateId : String
+    , fields : Dict String String
+    }
+
+
+decodePropertyGroup : Json.Decode.Decoder PropertyGroup
+decodePropertyGroup =
+    let
+        decodeField =
+            Json.Decode.map2 (,)
+                (Json.Decode.field "name" Json.Decode.string)
+                (Json.Decode.field "value" Json.Decode.string)
+    in
+    Pipeline.decode PropertyGroup
+        |> Pipeline.required "template_id" Json.Decode.string
+        |> Pipeline.required "fields" (Json.Decode.map Dict.fromList <| Json.Decode.list decodeField)
+
+
 {-| Return value for `upload`
+
+**WARNING**: elm-dropbox may give the incorrect values for `size`,
+since Elm currently does not provide a way to parse and represent 64-bit integers.
+
 -}
 type alias UploadResponse =
-    {}
+    { name : String
+    , id : String
+    , clientModified : Date
+    , serverModified : Date
+    , rev : String
+    , size : Int -- XXX: should be UInt64
+    , pathLower : Maybe String
+    , pathDisplay : Maybe String
+    , parentSharedFolderId : Maybe String
+    , mediaInfo : Maybe MediaInfo
+    , sharingInfo : Maybe FileSharingInfo
+    , propertyGroups : Maybe (List PropertyGroup)
+    , hasExplicitSharedMembers : Maybe Bool
+    , contentHash : Maybe String
+    }
+
+
+decodeUploadResponse : Json.Decode.Decoder UploadResponse
+decodeUploadResponse =
+    Pipeline.decode UploadResponse
+        |> Pipeline.required "name" Json.Decode.string
+        |> Pipeline.required "id" Json.Decode.string
+        |> Pipeline.required "client_modified" Json.Decode.Extra.date
+        |> Pipeline.required "server_modified" Json.Decode.Extra.date
+        |> Pipeline.required "rev" Json.Decode.string
+        |> Pipeline.required "size" Json.Decode.int
+        |> Pipeline.optional "path_lower" (Json.Decode.nullable Json.Decode.string) Nothing
+        |> Pipeline.optional "path_display" (Json.Decode.nullable Json.Decode.string) Nothing
+        |> Pipeline.optional "parent_shared_folder_id" (Json.Decode.nullable Json.Decode.string) Nothing
+        |> Pipeline.optional "media_info" (Json.Decode.nullable decodeMediaInfo) Nothing
+        |> Pipeline.optional "sharing_info" (Json.Decode.nullable decodeFileSharingInfo) Nothing
+        |> Pipeline.optional "property_groups" (Json.Decode.nullable <| Json.Decode.list decodePropertyGroup) Nothing
+        |> Pipeline.optional "has_explicit_shared_members" (Json.Decode.nullable Json.Decode.bool) Nothing
+        |> Pipeline.optional "content_hash" (Json.Decode.nullable Json.Decode.string) Nothing
 
 
 {-| Create a new file with the contents provided in the request.
@@ -317,7 +487,7 @@ upload auth info =
             Http.stringBody "application/octet-stream" info.content
 
         decoder =
-            Json.Decode.succeed {}
+            decodeUploadResponse
 
         dropboxArg =
             Json.Encode.encode 0 <|
